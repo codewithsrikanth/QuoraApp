861b4d2a75336afb90320c64576bae67f71903df
Srikanth Rallabandi <69148097+codewithsrikanth@users.noreply.github.com>
2020-08-24 15:55:44 +0530

    Database Backup

---
+++ Quora_Initial_DBQuries.sql
@@ -0,0 +1,76 @@
+create database QuoraDatabase
+go
+
+use QuoraDatabase
+go
+
+create table Categories(
+CategoryID int primary key identity(1,1),
+CategoryName nvarchar(max))
+go
+
+create table Users(
+UserID int primary key identity(1,1),
+Email nvarchar(max),
+PasswordHash nvarchar(max),
+Name nvarchar(max),
+Mobile nvarchar(max),
+IsAdmin bit default(0))
+go
+
+create table Questions(
+QuestionID int primary key identity(1,1),
+QuestionName nvarchar(max),
+QuestionDateAndTime datetime,
+UserID int references Users(UserID) on delete cascade,
+CategoryID int references Categories(CategoryID) on delete cascade,
+VotesCount int,
+AnswersCount int,
+ViewsCount int)
+go
+
+create table Answers(
+AnswerID int primary key identity(1,1),
+AnswerText nvarchar(max),
+AnswerDateAndTime datetime,
+UserID int references Users(UserID),
+QuestionID int references Questions(QuestionID) on delete cascade,
+VotesCount int)
+go
+
+create table Votes(
+voteID int primary key identity(1,1),
+UserID int references Users(UserID),
+AnswerID int references Answers(AnswerID) on delete cascade,
+VoteValue int)
+go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
