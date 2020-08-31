35ed07d6c371575e2145fa277fe9d338e3639ff4
Srikanth Rallabandi <69148097+codewithsrikanth@users.noreply.github.com>
2020-08-31 15:51:08 +0530

    Latest Repository

---
+++ QuoraApp.Repository/QuestionsRepository.cs
@@ -0,0 +1,97 @@
+﻿using QuoraApp.DomainModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuoraApp.Repository
+{
+    public interface IQuestionRepository
+    {
+        void InsertQuestion(Question q);
+        void UpdateQuestionDetails(Question q);
+        void UpdateQuestionVotesCount(int qid, int value);
+        void UpdateQuestionAnswersCount(int qid, int value);
+        void UpdateQuestionViewsCount(int qid, int value);
+        void DeleteQuestion(int qid);
+        List<Question> GetQuestions();
+        List<Question> GetQuestionByQuestionID(int QuestionID);
+    }
+    public class QuestionsRepository : IQuestionRepository
+    {
+        QuoraDBDataContext db;
+
+        public QuestionsRepository()
+        {
+            db = new QuoraDBDataContext();
+        }
+
+        public void InsertQuestion(Question q)
+        {
+            db.Questions.Add(q);
+            db.SaveChanges();
+        }
+
+        public void UpdateQuestionDetails(Question q)
+        {
+            Question qt = db.Questions.Where(temp => temp.QuestionID == q.QuestionID).FirstOrDefault();
+            if (qt != null)
+            {
+                qt.QuestionName = q.QuestionName;
+                qt.QuestionDateAndTime = q.QuestionDateAndTime;
+                qt.CategoryID = q.CategoryID;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionVotesCount(int qid, int value)
+        {
+            Question qt = db.Questions.Where(temp => temp.QuestionID == qid).FirstOrDefault();
+            if (qt != null)
+            {
+                qt.VotesCount += value;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionAnswersCount(int qid, int value)
+        {
+            Question qt = db.Questions.Where(temp => temp.QuestionID == qid).FirstOrDefault();
+            if (qt != null)
+            {
+                qt.AnswersCount += value;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionViewsCount(int qid, int value)
+        {
+            Question qt = db.Questions.Where(temp => temp.QuestionID == qid).FirstOrDefault();
+            if (qt != null)
+            {
+                qt.ViewsCount += value;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteQuestion(int qid)
+        {
+            Question qt = db.Questions.Where(temp => temp.QuestionID == qid).FirstOrDefault();
+            if (qt != null)
+            {
+                db.Questions.Remove(qt);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Question> GetQuestions()
+        {
+            List<Question> qt = db.Questions.OrderByDescending(temp => temp.QuestionDateAndTime).ToList();
+            return qt;
+        }
+
+        public List<Question> GetQuestionByQuestionID(int QuestionID)
+        {
+            List<Question> qt = db.Questions.Where(temp => temp.QuestionID == QuestionID).ToList();
+            return qt;
+        }
+    }
+}
---
+++ QuoraApp.Repository/VotesRepository.cs
@@ -0,0 +1,38 @@
+﻿using QuoraApp.DomainModels;
+using System.Linq;
+
+namespace QuoraApp.Repository
+{
+    public interface IVotesRepository
+    {
+        void UpdateVote(int aid, int uid, int value);
+    }
+    public class VotesRepository : IVotesRepository
+    {
+        QuoraDBDataContext db;
+
+        public VotesRepository()
+        {
+            db = new QuoraDBDataContext();
+        }
+
+        public void UpdateVote(int aid, int uid, int value)
+        {
+            int updateValue;
+            if (value > 0) updateValue = 1;
+            else if (value < 0) updateValue = -1;
+            else updateValue = 0;
+            Vote vote = db.Votes.Where(temp => temp.AnswerID == aid && temp.UserID == uid).FirstOrDefault();
+            if (vote != null)
+            {
+                vote.VoteValue = updateValue;
+            }
+            else
+            {
+                Vote newVote = new Vote() { AnswerID = aid, UserID = uid, VoteValue = updateValue };
+                db.Votes.Add(newVote);
+            }
+            db.SaveChanges();
+        }
+    }
+}
